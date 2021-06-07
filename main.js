66b507b1db59ff6892a859ae630626d3d93ec57d
Freakster22 <hiareeb@gmail.com>
2021-06-07 16:36:31 +0530

    Added Enter Event

--- main.js
+++ main.js
@@ -3,9 +3,20 @@ var ToDoItems = document.getElementById("ToDo-items");
 var input = document.getElementById("input");
 
 addToDo.addEventListener("click", function(){
+    if(input.value==""){
+        alert("You must enter some value");
+    }
     var para = document.createElement("p");
-    para.innerText = input.value;
+    para.classList.add("list");
+    para.innerText=input.value;
     ToDoItems.appendChild(para);
-    input.document.write("");
-
+    input.value='';
 })
+
+//For Enter keypress:
+input.addEventListener("keyup",event =>
+{
+    if(event.key=="Enter"){
+        addToDo.click();
+    }
+});
