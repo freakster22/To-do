5420cc641d145bb459aaa030f109a00b90d04e06
Freakster22 <hiareeb@gmail.com>
2021-06-07 17:49:04 +0530

    Added functionality to add and remove item from list

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var addToDo = document.getElementById("add");
 var ToDoItems = document.getElementById("ToDo-items");
 var input = document.getElementById("input");
 
-addToDo.addEventListener("click", function(){
+addToDo.addEventListener("click", function () {
     if(input.value==""){
         alert("You must enter some value");
     }
@@ -11,8 +11,16 @@ addToDo.addEventListener("click", function(){
     para.innerText=input.value;
     ToDoItems.appendChild(para);
     input.value='';
-})
-
+    para.addEventListener('click', evt =>
+        {
+            para.style.textDecoration='line-through'
+            para.style.color='rgb(200,180,145)'
+        });
+    para.addEventListener('dblclick', evt=>
+    {
+        ToDoItems.removeChild(para);
+    })
+});
 //For Enter keypress:
 input.addEventListener("keyup",event =>
 {
